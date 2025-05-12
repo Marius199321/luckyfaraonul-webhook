ae33915a45823c16bb24c75a04f2eabccf0c78e2
Marius199321 <marius@example.com>
2025-05-12 19:56:12 +0100

    Retry deploy: CORS fix

--- api/createCheckoutSession.js
+++ api/createCheckoutSession.js
@@ -14,7 +14,6 @@ export default async function handler(req, res) {
     }
 
     // restul codului tău...
-
     const { fullName, phone, email, address, country, productId, productName, qty } = req.body;
 
     try {
